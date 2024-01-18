38be19f0f963e022a4325ea031e68212c7f956f8
d3dinsy1d <Axeev207@gmail.com>
2024-01-18 16:39:22 +0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,25 @@
+
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
+9
+	style.css,4\6\46e3381e9569c09e85a30b9e5a2af7525ee9fd78
+9
+	script.js,3\1\313d6864fa48b411d082f7692efd0c0892788fc4
+7
+IV.html,5\3\530d48aed7fc453904514e798a8b1df671bc5366
+:
+
+price.html,8\3\83f01a4cc887e0efebd66e94a00a1517b251f507
+C
+assets/nootropy.svg,8\d\8d953a488f16a90530a75d8160a909858fe7514c
+E
+assets/decoration.svg,d\8\d8764e3c5aa1b48c6cc2de5320c13ca51c29f781
+F
+assets/diagnostics.svg,5\e\5ee994b3ab98e6af5d6ede6375ee9ee1148c5c75
+C
+assets/anonymus.svg,8\2\8293116a962e3b77b544dd7ac82517406f15e80c
+?
+assets/drug.svg,2\3\23ab1198946a6bccd89ed569803db8c415943664
+@
+assets/honor.svg,c\6\c69e48053a6831763bd0cc61a922622f223c58e0
---
+++ script.js
@@ -0,0 +1,37 @@
+function opentab(tabname, sectionIndex){
+    let tabcontent=document.getElementsByClassName("tabs")[sectionIndex].getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+
+    let tablinks = document.getElementsByClassName("tabs")[sectionIndex].getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    }
+    document.getElementById(tabname).classList.add("active");
+    event.currentTarget.className += " active";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.accordion section');
+
+    sections.forEach(section => {
+        const header = section.querySelector('h3');
+        const content = section.querySelector('div');
+
+        header.addEventListener('click', () => {
+            sections.forEach(s => {
+                if (s !== section) {
+                    s.classList.remove('active');
+                }
+            });
+
+            if (section.classList.contains('active')) {
+                section.classList.remove('active');
+            } else {
+                section.classList.add('active');
+            }
+        });
+    });
+});
